d88fc479cdb53bb4055dbc333959d6ca48bae9c7
unknown <willroymyles@yahoo.com>
2022-04-13 23:37:53 -0700

    Added add button

--- lib/frontend/pages/main/view.mainPage.dart
+++ lib/frontend/pages/main/view.mainPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wrg3/frontend/pages/posts/view.posts.dart';
 
@@ -7,11 +8,48 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: InkWell(
+        onTap: () {},
+        child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(color: Colors.transparent, width: 0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .secondary
+                          .withOpacity(.7),
+                      blurRadius: 5,
+                      offset: Offset(5, 5))
+                ]),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            )),
+      ),
+      // bottomNavigationBar: Container(
+      //   child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+      //     Container(
+      //       padding: EdgeInsets.all(10),
+      //       margin: EdgeInsets.all(10),
+      //       decoration: BoxDecoration(
+      //           color: Theme.of(context).colorScheme.secondary,
+      //           borderRadius: BorderRadius.circular(100)),
+      //       child: Icon(CupertinoIcons.add),
+      //     )
+      //   ]),
+      // ),
       body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return [
               SliverAppBar(
                 title: Text("home page"),
+                backgroundColor: Colors.transparent,
+                primary: true,
+                leading: Icon(CupertinoIcons.text_justifyright),
               )
             ];
           },
